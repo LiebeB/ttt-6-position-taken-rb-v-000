99cbbe832b3885f67a77ead97b4d60df61487fb1
Liebe Brand <liebebrand@gmail.com>
2018-08-01 08:41:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,11 @@
 # code your #position_taken? method here!
 
 puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
